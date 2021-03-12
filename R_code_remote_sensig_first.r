3c33add9a7e3b5db7cafb1aebc871235d0ca7e76
francescomessieri96 <80513697+francescomessieri96@users.noreply.github.com>
2021-03-12 11:00:09 +0100

    Create R_code_remote_sensig_first.r

---
+++ R_code_remote_sensig_first.r
@@ -0,0 +1,7 @@
+# My first code in R for remote sensig!!!
+# il mio primo codice in R per il telerilevamento!!
+# install.packages("raster")
+setwd("C:/lab/")
+library(raster)
+p224r63_2011<-brick("p224r63_2011_masked.grd
+plot(p224r63_2011)
