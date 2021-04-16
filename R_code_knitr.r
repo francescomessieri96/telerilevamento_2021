516fc307bbb2eb3391b279767c7f04b276005be7
francescomessieri96 <80513697+francescomessieri96@users.noreply.github.com>
2021-04-16 09:56:17 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -3,3 +3,4 @@ setwd("C:/lab/")
 #knitr, che va a prendersi un codice all’esterno e lo porta dentro R 
 #dove genera un report che salverà dentro alla cartella dove abbiamo preso il codice. 
 library(knitr)
+stitch("prova_.docx", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
