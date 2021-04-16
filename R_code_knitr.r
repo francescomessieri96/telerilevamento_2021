c42a42b3871995ecc518ab68006b24e5047e13b3
francescomessieri96 <80513697+francescomessieri96@users.noreply.github.com>
2021-04-16 09:32:35 +0200

    Create R_code_knitr.r

---
+++ R_code_knitr.r
@@ -0,0 +1,5 @@
+#R_code_knitr.r
+setwd("C:/lab/")
+#knitr, che va a prendersi un codice all’esterno e lo porta dentro R 
+#dove genera un report che salverà dentro alla cartella dove abbiamo preso il codice. 
+library(knitr)
