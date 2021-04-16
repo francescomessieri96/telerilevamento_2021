ce04a7c1e5603389b34d7f8cfbf6d97c16520246
francescomessieri96 <80513697+francescomessieri96@users.noreply.github.com>
2021-04-16 10:19:51 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -3,4 +3,4 @@ setwd("C:/lab/")
 #knitr, che va a prendersi un codice all’esterno e lo porta dentro R 
 #dove genera un report che salverà dentro alla cartella dove abbiamo preso il codice. 
 library(knitr)
-stitch("prova_.docx", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+stitch("R_code_greenland.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
