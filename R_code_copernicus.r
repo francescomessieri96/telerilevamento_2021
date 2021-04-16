f6ac3a1fa1e27b115cba8e8f2efc4cc617588b11
francescomessieri96 <80513697+francescomessieri96@users.noreply.github.com>
2021-04-16 08:33:47 +0200

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -5,10 +5,17 @@ library(raster)
 #install.packages("ncdf4")
 setwd("C:/lab/")
 #dato che carico un solo layer uso la funzione raster non la funzione brick
-NDVI<-raster("c_gls_NDVI_202005210000_GLOBE_PROBAV_V3.0.1.nc")
+NDVI<-raster("c_gls_NDVI_202006110000_GLOBE_PROBAV_V2.2.1.nc")
 #cambio la scala di colri
-cl <-colorRampPalette(c("yellow", "green", "dark green"))(100)
+cl <-colorRampPalette(c("yellow", "green", "dark green"))(255)
 plot(NDVI, col=cl)
 #ricampioniamo l'immagine, resampling
 NDVIres<-aggregate(NDVI, fact=100)
 plot(NDVIres, col=cl, main="NDVI maggio 2020")
+#16/06/2021
+setwd("C:/lab/")
+library(raster)
+install.packages("knitr")
+install.packages("RStoolbox")
+library(knitr)
+library(RStoolbox)
